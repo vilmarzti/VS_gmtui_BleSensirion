b44b77c6eaeb15e24772adc5ddc88b3a90602fe6
vilmarzti <vilmarti@student.ethz.ch>
2016-10-11 23:21:57 +0200

    onegraph

--- app/src/main/java/ch/ethz/inf/vs/a1/gmtui/ble/DeviceActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/gmtui/ble/DeviceActivity.java
@@ -1,5 +1,6 @@
 package ch.ethz.inf.vs.a1.gmtui.ble;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
@@ -8,46 +9,49 @@ import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 
 public class DeviceActivity extends AppCompatActivity {
-    private GraphContainer graphContainer;
-    private int x=0;
+    private GraphView graphView;
+    private LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
     private BluetoothDevice device;
     private BluetoothGatt humibluetoothGatt;
-    private BluetoothGatt tempbluetoothGatt;
     private TextView textview;
+    private Activity act;
     private BluetoothGattService humidity;
+    private long starttime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
 
+        graphView = (GraphView) findViewById(R.id.graphview);
         textview = (TextView) findViewById(R.id.connection_status);
         textview.setText(R.string.distconnected);
 
+        act = this;
+
         Intent intent = getIntent();
         device = intent.getParcelableExtra("device");
-        tempbluetoothGatt = device.connectGatt(this, false, humidCallback);
-        tempbluetoothGatt.connect();
-
-        GraphView graphView = new GraphView(this);
-        graphView.setTag("myGraphView");
-
-        ViewGroup layout = (ViewGroup)  findViewById(R.id.activity_device);
-        graphContainer = new GraphContainerImpl(graphView, 2, 40);
-
-        layout.addView(graphView);
+        humibluetoothGatt = device.connectGatt(this, true, humidCallback);
+        humibluetoothGatt.connect();
+        starttime = System.currentTimeMillis();
     }
 
     private float convertRawValue(byte[] raw) {
@@ -55,7 +59,7 @@ public class DeviceActivity extends AppCompatActivity {
         return wrapper.getFloat();
     }
 
-    private final BluetoothGattCallback humidCallback = new BluetoothGattCallback() {
+    private  BluetoothGattCallback humidCallback = new BluetoothGattCallback() {
 
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newstate){
@@ -69,69 +73,40 @@ public class DeviceActivity extends AppCompatActivity {
 
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic){
-            float  a = convertRawValue(characteristic.getValue());
-            graphContainer.addValues(x, new float[]{a});
+            final float a = convertRawValue(characteristic.getValue());
+            act.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    double currentsec = (System.currentTimeMillis()- starttime)/1000.0;
+                    series.appendData(new DataPoint(currentsec, a), false, 100);
+                    graphView.addSeries(series);
+                }
+            });
         }
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt,int status){
+            BluetoothGattCharacteristic humidityCharacteristic;
 
             if(status == BluetoothGatt.GATT_SUCCESS){
                 humidity = gatt.getService(SensirionSHT31UUIDS.UUID_HUMIDITY_SERVICE);
 
-                BluetoothGattCharacteristic humidityCharacteristic = humidity.getCharacteristic(SensirionSHT31UUIDS.UUID_HUMIDITY_CHARACTERISTIC);
+                humidityCharacteristic = humidity.getCharacteristic(SensirionSHT31UUIDS.UUID_HUMIDITY_CHARACTERISTIC);
+
                 BluetoothGattDescriptor descriptor = humidity.getCharacteristic(SensirionSHT31UUIDS.UUID_HUMIDITY_CHARACTERISTIC).getDescriptors().get(1);
                 descriptor.setValue(new byte[]{1});
 
                 gatt.writeDescriptor(descriptor);
 
-                BluetoothGattCharacteristic test = new BluetoothGattCharacteristic(SensirionSHT31UUIDS.UUID_HUMIDITY_CHARACTERISTIC,
+                BluetoothGattCharacteristic newChar = new BluetoothGattCharacteristic(SensirionSHT31UUIDS.UUID_HUMIDITY_CHARACTERISTIC,
                         humidityCharacteristic.getProperties(),
                         BluetoothGattCharacteristic.PERMISSION_WRITE);
 
-                humidity.addCharacteristic(test);
-                gatt.setCharacteristicNotification(test, true);
-            }
-        }
-    };
-
-    private final BluetoothGattCallback temptCallback = new BluetoothGattCallback() {
-
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newstate){
-            if(newstate == BluetoothProfile.STATE_CONNECTED){
-                gatt.discoverServices();
-            }
-            else{
-                //textview.setText(R.string.distconnected);
+                humidity.addCharacteristic(newChar);
+                gatt.setCharacteristicNotification(newChar, true);
             }
         }
 
-        @Override
-        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic){
-            float  a = convertRawValue(characteristic.getValue());
-
-        }
-        @Override
-        public void onServicesDiscovered(BluetoothGatt gatt,int status){
-
-            if(status == BluetoothGatt.GATT_SUCCESS){
-                humidity = gatt.getService(SensirionSHT31UUIDS.UUID_TEMPERATURE_SERVICE);
-
-                BluetoothGattCharacteristic humidityCharacteristic = humidity.getCharacteristic(SensirionSHT31UUIDS.UUID_TEMPERATURE_CHARACTERISTIC);
-                BluetoothGattDescriptor descriptor = humidity.getCharacteristic(SensirionSHT31UUIDS.UUID_TEMPERATURE_CHARACTERISTIC).getDescriptors().get(1);
-                descriptor.setValue(new byte[]{1});
-
-                gatt.writeDescriptor(descriptor);
-
-                BluetoothGattCharacteristic test = new BluetoothGattCharacteristic(SensirionSHT31UUIDS.UUID_TEMPERATURE_CHARACTERISTIC,
-                        humidityCharacteristic.getProperties(),
-                        BluetoothGattCharacteristic.PERMISSION_WRITE);
-
-                humidity.addCharacteristic(test);
-                gatt.setCharacteristicNotification(test, true);
-            }
-        }
     };
 
 }
